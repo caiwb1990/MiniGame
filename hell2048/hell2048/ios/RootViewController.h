706acde92ddae77e6b090d147f48aa8947033637
Hellc <caiwb1990@gmail.com>
2014-12-01 21:12:32 +0800

    + add proj files

---
+++ hell2048/hell2048/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  hell2048AppController.h
+//  hell2048
+//
+//  Created by wenbin.cai on 14-12-1.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
