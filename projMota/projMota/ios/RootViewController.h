1f63a1ed8674942951ca894818dd1f886034f8eb
Hellc <caiwb1990@gmail.com>
2014-12-25 19:58:49 +0800

    + add new Proj Mota

---
+++ projMota/projMota/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  projMotaAppController.h
+//  projMota
+//
+//  Created by wenbin.cai on 14-12-25.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
