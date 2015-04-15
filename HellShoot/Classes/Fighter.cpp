8868ab1e034362a124afe048ff376bef281bbde0
Hellc <caiwb1990@gmail.com>
2015-04-15 23:48:03 +0800

    + add fighter

---
+++ HellShoot/Classes/Fighter.cpp
@@ -0,0 +1,9 @@
+//
+//  Fighter.cpp
+//  HellShoot
+//
+//  Created by wenbin.cai on 15/4/15.
+//
+//
+
+#include "Fighter.h"
---
+++ HellShoot/Classes/Fighter.h
@@ -0,0 +1,14 @@
+//
+//  Fighter.h
+//  HellShoot
+//
+//  Created by wenbin.cai on 15/4/15.
+//
+//
+
+#ifndef __HellShoot__Fighter__
+#define __HellShoot__Fighter__
+
+#include <stdio.h>
+
+#endif /* defined(__HellShoot__Fighter__) */
