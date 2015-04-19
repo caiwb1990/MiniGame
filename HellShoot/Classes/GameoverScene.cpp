ffe5a550d2ff58583849b67303af0a4ae64d3a4d
Hellc <caiwb1990@gmail.com>
2015-04-19 16:03:06 +0800

    + add Gameover scene

---
+++ HellShoot/Classes/GameoverScene.cpp
@@ -0,0 +1,9 @@
+//
+//  GameoverScene.cpp
+//  HellShoot
+//
+//  Created by wenbin.cai on 15/4/19.
+//
+//
+
+#include "GameoverScene.h"
---
+++ HellShoot/Classes/GameoverScene.h
@@ -0,0 +1,14 @@
+//
+//  GameoverScene.h
+//  HellShoot
+//
+//  Created by wenbin.cai on 15/4/19.
+//
+//
+
+#ifndef __HellShoot__GameoverScene__
+#define __HellShoot__GameoverScene__
+
+#include <stdio.h>
+
+#endif /* defined(__HellShoot__GameoverScene__) */
